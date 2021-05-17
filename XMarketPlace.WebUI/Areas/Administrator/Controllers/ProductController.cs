fc0826fea38714883585879aa4b9025298407159
karahanba <batuhankarahan@hotmail.com>
2021-05-17 12:20:48 +0300

    Upload Model created / ProductController created for Administrator area

---
+++ XMarketPlace.WebUI/Areas/Administrator/Controllers/ProductController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using XMarketPlace.Core.Service;
+using XMarketPlace.Model.Entities;
+using XMarketPlace.WebUI.Areas.Administrator.Models;
+
+namespace XMarketPlace.WebUI.Areas.Administrator.Controllers
+{
+    [Area("Administrator"), Authorize]
+    public class ProductController : Controller
+    {
+        private IHostingEnvironment _env;
+        private readonly ICoreService<Product> _ps;
+        private readonly ICoreService<Category> _cs;
+
+        public ProductController(ICoreService<Product> ps, ICoreService<Category> cs, IHostingEnvironment env)
+        {
+            _ps = ps;
+            _cs = cs;
+            _env = env;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_ps.GetAll());
+        }
+
+        public IActionResult Insert()
+        {
+            ViewBag.Categories = new SelectList(_cs.GetActive(), "ID", "CategoryName");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Insert(Product prdct, List<IFormFile> files)
+        {
+            if (ModelState.IsValid)
+            {
+                // Since Product doesn't have UserID as foreign key, below line is unnecessary
+                //prdct.UserID = Guid.Parse(User.Claims.FirstOrDefault(x => x.Type == "ID").Value);
+
+                bool imgResult;
+                string imgPath = Upload.ImageUpload(files, _env, out imgResult);
+
+                if (imgResult)
+                {
+                    prdct.ImagePath = imgPath;
+                }
+                else
+                {
+                    TempData["Message"] = "Resim yükleme esnasında hata Oluştu";
+                }
+
+                bool result = _ps.Add(prdct);
+                if (result)
+                {
+                    // true ise
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    // false ise
+                    TempData["Message"] = "Kayıt esnasında bilinmeyen bir hata oluştu";
+                }
+            }
+            else
+            {
+                TempData["Message"] = "Hata Oluştu";
+            }
+
+            return View(prdct);
+        }
+
+        public IActionResult Update(Guid id)
+        {
+            ViewBag.Categories = new SelectList(_cs.GetActive(), "ID", "CategoryName");
+            return View(_ps.GetById(id));
+        }
+
+        [HttpPost]
+        public IActionResult Update(Product prdct)
+        {
+            if (ModelState.IsValid)
+            {
+                var updated = _ps.GetById(prdct.ID);
+                updated.ProductName = prdct.ProductName;
+                updated.ProductDetail = prdct.ProductDetail;
+                updated.ProductSummary = prdct.ProductSummary;
+                updated.UnitPrice = prdct.UnitPrice;
+                updated.UnitsInStock = prdct.UnitsInStock;
+
+                bool result = _ps.Update(updated);
+                if (result)
+                {
+                    // true ise
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    // false ise
+                    TempData["Message"] = "Kayıt esnasında bilinmeyen bir hata oluştu";
+                }
+            }
+            else
+            {
+                TempData["Message"] = "Hata Oluştu";
+            }
+
+            return View(prdct);
+        }
+
+        public IActionResult Delete(Guid id)
+        {
+            bool result = _ps.Remove(_ps.GetById(id));
+            if (result)
+            {
+                // true ise
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // false ise
+                TempData["Message"] = "Silme esnasında bilinmeyen bir hata oluştu";
+            }
+
+            return View();
+        }
+
+        public IActionResult Activate(Guid id)
+        {
+            bool result = _ps.Activate(id);
+            if (result)
+            {
+                // true ise
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // false ise
+                TempData["Message"] = "Aktivasyon esnasında bilinmeyen bir hata oluştu";
+            }
+
+            return View();
+        }
+    }
+}
---
+++ XMarketPlace.WebUI/Areas/Administrator/Models/Upload.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XMarketPlace.WebUI.Areas.Administrator.Models
+{
+    public class Upload
+    {
+        
+        public static string ImageUpload(List<IFormFile> files, IHostingEnvironment env, out bool result)
+        {
+            result = false;
+            var uploads = Path.Combine(env.WebRootPath, "uploads"); // Path.Combine ile pathleri birleştirdik. Yani uygulamanızın adı ile resimleri yükleyeceğiniz klasörü birleşitirip tek parça haline getirdik. Örnek; XMarketPlace/XMarketPlace.WebUI/uploads
+
+            foreach (var file in files)
+            {
+                if (file.ContentType.Contains("image"))
+                {
+                    if (file.Length <= 2097152) // Resim dosyası 2MB veya daha düşük bir boyutta ise yüklemeye geç
+                    {
+                        string uniqueName = $"{Guid.NewGuid().ToString().Replace("-", "_").ToLower()}.{file.ContentType.Split('/')[1]}"; // yüklenecek resim için benzersiz bir isim oluşturuldu. file.ContentType.Split('/')[1] ifadesi ile yüklenecek dosyanın uzantısını elde ediyoruz
+
+                        var filePath = Path.Combine(uploads, uniqueName);
+
+
+                        using (var fileStream = new FileStream(filePath, FileMode.Create))
+                        {
+                            file.CopyTo(fileStream); 
+                            result = true;
+                            return filePath.Substring(filePath.IndexOf("\\uploads\\")); 
+                        }
+                    }
+                    else
+                    {
+                        return "2MB'dan büyük resim dosyası yüklenemez";
+                    }
+                }
+                else
+                {
+                    return "Lütfen sadece resim dosyası yükleyin";
+                }
+            }
+
+            return "";
+        }
+    }
+}
