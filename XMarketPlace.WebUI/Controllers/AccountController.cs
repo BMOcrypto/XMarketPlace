e0c1b31b69eb8f85cc14a460657210edc3e3ee5c
karahanba <batuhankarahan@hotmail.com>
2021-06-20 02:36:55 +0300

    Image post through form is fixed

--- XMarketPlace.WebUI/Controllers/AccountController.cs
+++ XMarketPlace.WebUI/Controllers/AccountController.cs
@@ -4,19 +4,24 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using XMarketPlace.Core.Service;
 using XMarketPlace.Model.Entities;
+using XMarketPlace.WebUI.Areas.Administrator.Models;
 
 namespace XMarketPlace.WebUI.Controllers
 {
     public class AccountController : Controller
     {
+        private IHostingEnvironment _env;
         private readonly ICoreService<User> _us;
 
-        public AccountController(ICoreService<User> us)
+        public AccountController(ICoreService<User> us, IHostingEnvironment env)
         {
             _us = us;
+            _env = env;
         }
         
         public IActionResult Login() 
@@ -65,11 +70,30 @@ namespace XMarketPlace.WebUI.Controllers
         }
 
         [HttpPost]
-        public IActionResult SignUp(User user)
+        public IActionResult SignUp(User user, List<IFormFile> files)
         {
 
-            _us.Add(user);
-            _us.Save();
+            if (ModelState.IsValid)
+            {
+                bool imgResult;
+                string imgPath = Upload.ImageUpload(files, _env, out imgResult);
+
+                if (imgResult)
+                {
+                    user.ImageUrl = imgPath;
+                }
+                else
+                {
+                    TempData["Message"] = "Resim yükleme esnasında hata oluştu";
+                }
+                _us.Add(user);
+                _us.Save();
+            }
+            else
+            {
+                TempData["Message"] = "Hata oluştu. Lütfen daha sonra tekrar deneyin";
+            }
+
 
 
             return RedirectToAction("Login", "Account");
